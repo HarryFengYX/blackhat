595cb87eba55a0cdd6bfcfc12fff65b8cf66c7c7
Alex Zhang <502092439@qq.com>
2017-07-02 00:27:18 +0800

    Update udpClient.py

--- udpClient.py
+++ udpClient.py
@@ -1,11 +1,11 @@
 from socket import *
 
 target_host = "127.0.0.1"
-taget_port = 80
+target_port = 80
 
-client = socket(type=socket.SOCK_DGRAM)
+client = socket(type=SOCK_DGRAM)
 
-client.sendto(b"AAABBBCCC", (target_host, taget_port))
+client.sendto(b"AAABBBCCC", (target_host,target_port))
 
 data, addr = client.recvfrom(4096)
 
