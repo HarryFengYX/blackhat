9bc37cd09b6c2edf33eb2b9e91c1f528f37ab3f2
Alex Zhang <502092439@qq.com>
2017-07-02 00:26:27 +0800

    Update tcpServer.py

--- tcpServer.py
+++ tcpServer.py
@@ -1,14 +1,14 @@
 from socket import *
 from threading import *
 
-bibd_ip = "0.0.0.0"
+bind_ip = "0.0.0.0"
 bind_port = 9999
 
 server = socket()
-server.bind((bibd_ip, bind_port))
+server.bind((bind_ip, bind_port))
 server.listen(5)
 
-print("[*] Listening on %s:%d" % (bibd_ip, bind_port))
+print("[*] Listening on %s:%d" % (bind_ip, bind_port))
 
 def handle_client(client_socket):
     request = client_socket.recv(1024)
